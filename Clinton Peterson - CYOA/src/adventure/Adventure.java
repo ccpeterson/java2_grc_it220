4022f045e8daedf69ac17d40983bc468a77a11e3
ccpeterson <clinton.c.peterson@gmail.com>
2017-11-15 20:05:49 -0800

    adjkhdfkjfkjdef

--- Clinton Peterson - CYOA/src/adventure/Adventure.java
+++ Clinton Peterson - CYOA/src/adventure/Adventure.java
@@ -1,5 +1,11 @@
 package adventure;
 
+/**
+ * Project: CYOA 
+ * 11/15/17
+ * @author Clinton Peterson
+ */
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
@@ -9,20 +15,30 @@ public class Adventure
 {
 	//fields
 	private String[] steps =  new String[1];
-	private int position;
+	private int position = 0;
 	
+	/**
+	 * 
+	 * @param fileName A string that is the path and filename of the text file containing the formatted story
+	 * @param lines An int that is the number of lines in the file
+	 */
 	public Adventure(String fileName, int lines)
-	{		
-		Scanner reader = null;
+	{	
+		//initializing the reader outside of the try/catch
+		Scanner reader = null; 
+		//copying the array to itself, but with the correct size
 		steps = Arrays.copyOf(steps, lines);
+		//try catch because i have to
 		try
 		{
+			//accesses the file given to the constructor
 			reader = new Scanner(new FileInputStream(fileName));
 		}
 		catch(FileNotFoundException ex)
 		{
 			System.out.println(ex);
 		}
+		//adds each line of the text file as an element of the steps array
 		while(reader.hasNextLine())
 		{
 			for (int i = 0; i < lines; i++)
@@ -30,23 +46,68 @@ public class Adventure
 				steps[i] = reader.nextLine();
 			}			
 		}
-		reader.close();
-		
-		
+		//close that reader!
+		reader.close();		
 	}
 	
+	/**
+	 * runs the story
+	 */
 	public void start()
 	{
 		Scanner console = new Scanner(System.in);
-		position = 0;
-		
-		boolean done = false;
-		while (!done)
+		//loops through until broken
+		while (true)
 		{
-			System.out.println(steps[position]);
-			int choice = console.nextInt();
-			position = choice;
+			// splits the line into 2, statement and options
+			String[] statement = steps[position].split(":");
+			//prints out the statement of the current position
+			System.out.println(statement[0]); 
+			//splits the options into the options an their numbers
+			String[] options = statement[1].split("=|\\|"); 
 			
+			//prints out the options, they should be the even indexed parts of the array, also adds numbers ahead of them
+			for (int i = 0; i < options.length / 2 ; i++) 
+			{
+				System.out.println(i+1 + " - " + options[i*2]);
+			}
+			
+			//checks if you are in the "dead" position
+			if (Integer.parseInt(options[1].trim()) == -1)
+			{
+				System.out.println("Game over, you lose!");
+				break;
+			}
+			
+			//checks if you are in the "win" position
+			if (Integer.parseInt(options[1].trim()) == -2)
+			{
+				System.out.println("Game over, you win!");
+				break;
+			}
+			
+			//gets the users choice and validating it
+			int choice = 0;
+			boolean done = false;
+			while (!done)
+			{
+				choice = console.nextInt();				
+				if (choice > 0 && choice <= options.length / 2)
+				{
+					done = true;
+				}				
+				else {
+					System.out.println("Please enter a valid response");				
+				}
+			}			
+			//int choice = console.nextInt();
+			
+			//uses that choice to get the corresponding page they want to go to
+			int choice2 = Integer.parseInt(options[choice*2-1].trim());
+			//sets position to the correct page
+			position = choice2;			
 		}
+		//close that console!
+		console.close();
 	}
 }
--- Clinton Peterson - CYOA/src/adventure/Driver.java
+++ Clinton Peterson - CYOA/src/adventure/Driver.java
@@ -1,25 +1,50 @@
 package adventure;
 
+/**
+ * Project: CYOA 
+ * 11/15/17
+ * @author Clinton Peterson
+ */
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Driver
 {
+	//console used for user input
 	private static Scanner console = new Scanner(System.in);
-
+	//int used for which story is to be used
+	private static int story = 0;
+	//string used for the filename of the story selected
+	private static String storyFile = "";
+	//int which will be the number of lines in the file
+	private static int numLines = 0;
+	
 	public static void main(String[] args)
-	{		
-		boolean done = false; //loops until it gets a good response for player type
-		int story = 0;
-		String storyFile = "";
-		int numLines = 0;
+	{	
+		//displays the menu
+		menu();
+		//creates a new adventure object with the info gathered in the menu
+		Adventure adventure1 = new Adventure(storyFile, numLines);
+		//start the adventure
+		adventure1.start();
+	}
+	
+	/**
+	 * Gets the user to pick the story they want to do, sets the storyFile and numLines 
+	 */
+	private static void menu()
+	{
+		//loops until it gets a good response for player type
+		boolean done = false; 
 		while (!done)
 		{
 			System.out.println("Which adventure do you wish to have?");
 			System.out.println("1) Zombies!");
 			System.out.println("2) Other Adventure!");
 			story = console.nextInt();
+			//these could be made to constants probably
 			if (story == 1)
 			{
 				storyFile = "files/zombies.txt";
@@ -34,31 +59,39 @@ public class Driver
 				System.out.println("Please enter a valid response");				
 			}
 		}
-		
+		//gotta do this since the file might not be there
 		try
 		{
-			numLines = howManyLines("files/zombies.txt");
+			numLines = howManyLines(storyFile);
 		}
 		catch(FileNotFoundException ex)
 		{
 			System.out.println(ex);
 		}
-		System.out.println("Number of lines: " + numLines);
-		
-		Adventure adventure1 = new Adventure(storyFile, numLines);
-		adventure1.start();
 	}
 	
-	public static int howManyLines(String filename) throws FileNotFoundException
+	/**
+	 * 
+	 * @param filename The file to be checked for line count
+	 * @return an int with the amount of lines in the file
+	 * @throws FileNotFoundException 
+	 */
+	private static int howManyLines(String filename) throws FileNotFoundException
 	{
+		//opening the file
 		Scanner reader = new Scanner(new FileInputStream(filename));
+		//int initialized at 0 to be used as a counter of lines in the file 
 		int lineCount = 0;
+		
+		//runs through the file until there are no more lines, increments the counter each time
 		while(reader.hasNextLine())
 		{
 			reader.nextLine();
 			lineCount++;
 		}
+		//close that reader!
 		reader.close();
+		
 		return lineCount;
 	}
 }
