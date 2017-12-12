5c4d702d58f23e1acf05170b38cbab2c4a77a5c8
ccpeterson <clinton.c.peterson@gmail.com>
2017-12-12 12:03:04 -0800

    dwadwad

--- Clinton Peterson - Josephus/src/game/JosephusCircle.java
+++ Clinton Peterson - Josephus/src/game/JosephusCircle.java
@@ -1,50 +1,171 @@
 package game;
 
+/**
+ * Project: Josephus Circle
+ * Date: 11 Dec 2017
+ * 
+ * @author Clinton Peterson
+ * 
+ */
+
+import exceptions.GameException;
+
 public class JosephusCircle
 {
-	//fields go here...
+	//fields
+	private int size;
+	private int maxPlayers;
+	private JosephusNode head;
+	private JosephusNode cursor;
 	
+	/**
+	 * 
+	 * @param playerCount
+	 */
 	public JosephusCircle(int playerCount)
 	{
 		//Creates a new JosephusCircle object with the maximum players given.
+		this.maxPlayers = playerCount;		
 	}
 	
-	public void addPlayer(String player)
+	/**
+	 * Adds a node to the linked list
+	 * @param player name of the player
+	 * @throws GameException
+	 */
+	public void addPlayer(String player) throws GameException
 	{
 		//Adds a new player to the game from left-to-right in the 
 		//internal linked list.
+		if (size == maxPlayers)
+		{
+			throw new GameException();
+		}
+		if (head == null)
+		{
+			head = new JosephusNode(player,null,head);
+			cursor = head;
+		}
+		else
+		{
+			JosephusNode current = head;
+			while(current.getNext() != null)
+			{
+				current = current.getNext();
+			}
+			current.setNext(new JosephusNode(player,null,current));
+		}
+		size++;		
 	}
 	
+	/**
+	 * turns the list into a circle
+	 */
 	public void circularize()
 	{
 		//Converts the internal linked list that maintains this circle 
 		//of players into a circular linked list.
+		JosephusNode first = head;
+		JosephusNode current = head;
+		while(current.getNext() != null)
+		{
+			current = current.getNext();
+		}
+		current.setNext(first);
+		first.setPrevious(current);		
 	}
 	
+	/**
+	 * Moves the cursor pointer
+	 * @param direction right or left
+	 * @param steps how many steps
+	 * @return the player's name 
+	 */
 	public String moveCursor(Direction direction, int steps)
 	{
 		//Moves the current player (the cursor) from the current 
 		//position the given number of steps to the left or right
-		return null;
+		for (int i=0; i < steps; i++)
+		{
+			if (direction == Direction.RIGHT)
+			{
+				cursor = cursor.getNext();
+			}
+			if (direction == Direction.LEFT)
+			{
+				cursor = cursor.getPrevious();
+			}
+		}		
+		return cursor.getData().toString();
 	}
+	
+	/**
+	 * Removes the node at the current cursor pointer and moves the pointer in the direction of the game
+	 * @param direction right or left
+	 */
 	
 	public void removeAtCursor(Direction direction)
 	{
-		//Removes the player at the cursor position from the circle.
+		JosephusNode before = cursor.getPrevious();
+		JosephusNode after = cursor.getNext();
+		if (direction == Direction.RIGHT)
+		{
+			cursor = cursor.getNext();
+		}
+		if (direction == Direction.LEFT)
+		{
+			cursor = cursor.getPrevious();
+		}
+		before.setNext(after);
+		after.setPrevious(before);
+		size--;
 	}
 	
+	/**
+	 * Returns a string representing the internal state of the circle.
+	 * @return a string representing the internal state of the circle
+	 */
 	public String getCircle()
 	{
-		//Returns a string representing the internal state of the circle, 
-		//with the following format:
-		//<-- Player A <--> Player B <--> Player C <--> Player D -->
-		return null;
+		
+		String circle = "<--";
+		JosephusNode current = head;
+		for (int i = 0; i < size; i++)		
+		{
+			if (current == cursor)
+			{
+				circle = circle + " " + current.getData().toString().toUpperCase() + " ";
+			}
+			else
+			{	
+				circle = circle + " " + current.getData() + " ";
+			}
+			if (i < size -1)
+			{
+				circle = circle + "<-->";
+			}
+			current = current.getNext();
+			
+		}
+		circle = circle + "-->";
+		
+		return circle;
 	}
 	
+	/**
+	 * Returns true if the game is over. 
+	 * @return true if only one player remains in the circle
+	 */
 	public boolean isGameOver()
 	{
-		//Returns true if the game is over. That is, this method will 
-		//return true if only one player remains in the circle.
+		
+		if (size == 1)
+		{
+			return true;
+		}
+		else
+		{		
 		return false;
+		}
 	}
 }
--- Clinton Peterson - Josephus/src/game/JosephusNode.java
+++ Clinton Peterson - Josephus/src/game/JosephusNode.java
@@ -1,9 +1,89 @@
 package game;
 
+/**
+ * Project: Josephus Circle
+ * Date: 11 Dec 2017
+ * 
+ * @author Clinton Peterson
+ * 
+ */
+
 //A doubly-linked list node. This node class is not an inner class, 
 //so you will need to use getters/setters to access the fields in 
 //the class. They should not be public.
 public class JosephusNode
 {
+	private Object data;
+	private JosephusNode next;
+	private JosephusNode previous;
 	
+	public JosephusNode(Object data, JosephusNode next, JosephusNode previous)
+	{
+		this.data = data;
+		this.next = next;
+		this.previous = previous;
+	}
+	
+	/**
+	 * Gets the data for this node
+	 * @return The data object for this node
+	 */
+	public Object getData() 
+	{
+		return data;
+	}
+	
+	/**
+	 * Sets the data for this node
+	 * @param data The data object for this node
+	 */
+	public void setData(Object data) 
+	{
+		this.data = data;
+	}
+
+	/**
+	 * Gets the node this node's next pointer is pointing to
+	 * @return the node this node's next pointer is pointing to
+	 */
+	public JosephusNode getNext() 
+	{
+		return next;
+	}
+	
+	/**
+	 * Sets the node this node's next pointer is pointing to
+	 * @param next the node this node's next pointer is pointing to
+	 */
+	public void setNext(JosephusNode next) 
+	{
+		this.next = next;
+	}
+	
+	/**
+	 * Gets the node this node's previous pointer is pointing to
+	 * @return the node this node's previous pointer is pointing to
+	 */
+	public JosephusNode getPrevious() 
+	{
+		return previous;
+	}
+	
+	/**
+	 * Sets the node this node's previous pointer is pointing to
+	 * @param next the node this node's previous pointer is pointing to
+	 */
+	public void setPrevious(JosephusNode previous) 
+	{
+		this.previous = previous;
+	}
+	
+	/**
+	 * Returns a string about this node's data
+	 * @return a string about this node's data 
+	 */
+	public String toString()
+	{
+		return "Node: " + data.toString();
+	}	
 }
